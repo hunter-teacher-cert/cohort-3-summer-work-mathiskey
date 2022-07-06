fa706a7f6d420d8d5fd7c2829a659a0d613a0be7
Shana Elizabeth Henry <57843914+mathiskey@users.noreply.github.com>
2022-07-06 15:42:25 +0000

    added Count Neighbours

---
+++ programming/3/Cgol.java
@@ -0,0 +1,146 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Conway's Game of Life by Team AreWeSentientYet?
+ * Jihae Park
+ * collaborators: Seth Adams, Shana Elizabeth Henry, Marisa Shuman
+ */
+
+/**
+   The Rules of Life:
+
+   Survivals:
+   * A living cell with 2 or 3 living neighbours will survive for the next generation.
+
+   Deaths:
+   * Each cell with >3 neighbours will die from overpopulation.
+   * Every cell with <2 neighbours will die from isolation.
+
+   Births:
+   * Each dead cell adjacent to exactly 3 living neighbours is a birth cell. It will come alive next generation.
+
+   NOTA BENE:  All births and deaths occur simultaneously. Together, they constitute a single generation.
+*/
+
+public class Cgol
+{
+
+  //create, initialize, and return  empty board (all cells dead) 
+  public static char[][] createNewBoard( int rows, int cols ) //1
+  {
+    char[][] board = new char[rows][cols];                                                       
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        board[i][j]='-';
+      }
+    }
+    return board;
+  }
+
+
+  //print the board to the terminal 
+  public static void printBoard( char[][] board ) //3
+  {
+    for (int i = 0; i < board.length; i++) 
+    {
+      for (int j = 0; j < board[i].length; j++) //i instead of row??
+      {
+        System.out.print(board[i][j] + " ");
+        
+      }
+    System.out.println("");
+    }
+  }
+
+
+  //set cell (r,c) to val 
+  public static void setCell( char[][] board, int r, int c, char val ) //2
+  {
+    board [r][c] = val;
+  }
+
+
+  //return number of living neigbours of board[r][c] 
+  public static int countNeighbours( char[][] board, int r, int c ) //4
+  {
+    int count = 0;
+    int rows = board.length;
+    int cols = board[0].length;
+
+    for (int i=r-1; i<=r+1; i++) {
+      for (int j=c-1; j<=c+1; j++) {
+        //check to see if the current cell is the targeted cell. If it is, then continue with the incremented value? 
+        if (i == r && j == c){
+          continue;
+        }
+        if ((i >= 0 && i < rows) && (j >= 0 && j < cols)) {
+          //i != r
+            if (board[i][j] == 'X') {
+              count ++;
+            } 
+            if (board[r][c] == 'X'){
+              count -1;
+            }
+        }   
+      }
+    }
+    }
+    return count;
+  }
+
+    //start at the r to the top left -- iterate over that until it is at the bottom right. count -- think of it as creating a square of 8 around the specific indicated cell and is iterating over all of those cells.
+ 
+
+  /**
+     precond: given a board and a cell
+     postcond: return next generation cell state based on CGOL rules
+     (alive 'X', dead '-')
+  */
+  //check the conditions for live/dead
+  public static char getNextGenCell( char[][] board,int r, int c ) //5
+  {
+    int numLive = countNeigh
+  }
+
+
+  //generate and return a new board representing next generation
+  // public static char[][] generateNextBoard( char[][] board ) //6
+  // {
+
+  // }
+
+
+  public static void main( String[] args )
+  {
+    
+    char[][] board;
+    board = createNewBoard(5,5);
+
+    //breathe life into some cells:
+    setCell(board, 0, 0, 'X');
+    setCell(board, 0, 1, 'X');
+    setCell(board, 1, 0, 'X');
+    System.out.println("Gen X:");
+    printBoard(board);
+    System.out.println("--------------------------\n\n");
+    System.out.println(countNeighbours(board,0,0));
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // TASK:
+    // Once your initial version is running,
+    // try out different starting configurations of living cells...
+    // (Feel free to comment out the above three lines.)
+
+    System.out.println("Gen X:");
+    printBoard(board);
+    System.out.println("--------------------------\n\n");
+
+    board = generateNextBoard(board);
+
+    System.out.println("Gen X+1:");
+    printBoard(board);
+    System.out.println("--------------------------\n\n");
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+  }//end main()
+
+}//end class
