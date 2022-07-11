ddc903669ab1c3f8944dacd7d7ad6bdf99a0c785
Shana Elizabeth Henry <57843914+mathiskey@users.noreply.github.com>
2022-07-11 18:54:32 +0000

    added code

--- programming/6/sa/SuperArray.java
+++ programming/6/sa/SuperArray.java
@@ -1,4 +1,4 @@
-/**
+  /**
  * SuperArray by Room 10
  * Jing Xue
  * collaborators: Erwin Lara, Shana Elizabeth Henry, Joel Bianchi
@@ -18,109 +18,76 @@
 import java.io.*;
 import java.util.*;
 
-public class SuperArray
-{
+public class SuperArray {
   /**
-     SUBGOAL:
-     declare instance vars
-     ...and initialize?
-  */
+   * SUBGOAL: declare instance vars ...and initialize?
+   */
 
-
-  //instance vars
-  private int[] data;           // "interior"/"underlying" data container
-  private int numberElements;   // number of "meaningful" elements
-
+  // instance vars
+  private int[] data; // "interior"/"underlying" data container
+  private int numberElements; // number of "meaningful" elements
 
   // ~~~~~~~~~~~~~~~ CONSTRUCTORS ~~~~~~~~~~~~~~~
-  //overloaded constructor -- allows specification of initial capacity
-  public SuperArray( int size )
-  {
-    
-    //init underlying/inner storage of specified capacity
-    data = new int [size];
+  // overloaded constructor -- allows specification of initial capacity
+  public SuperArray(int size) {
 
-    //init instance vars
-    numberElements = 0; 
+    // init underlying/inner storage of specified capacity
+    data = new int[size];
+
+    // init instance vars
+    numberElements = 0;
 
   }
 
-  //default constructor -- initializes capacity to 10
-  public SuperArray()
-  {
-    //init underlying/inner storage of capacity 10
-    data = new int [10];
+  // default constructor -- initializes capacity to 10
+  public SuperArray() {
+    // init underlying/inner storage of capacity 10
+    data = new int[10];
 
-    //init instance vars
+    // init instance vars
     numberElements = 0;
   }
 
-
   // ~~~~~~~~~~~~~~~ METHODS ~~~~~~~~~~~~~~~
-  public void add( int value )
-  {
+  public void add(int value) {
     // test to see if we need to grow, then grow
-    if (numberElements >= data.length){
+    if (numberElements == data.length) {   //narrowest condition (not including >)
       grow();
     }
-    
-    /**
-       IMPORTANT:
-       This is the first code that should run in this method
-       but the last code you should write.
-       Implement the rest of this method first,
-       then only write this section once the rest is tested and working.
-    */
-    /* YOUR SIMPLE+SMART CODE HERE */
-  
+
     // add item to the end of the array
-    /* YOUR SIMPLE+SMART CODE HERE */
     data[numberElements] = value;
 
     // increment numberElements
-    /* YOUR SIMPLE+SMART CODE HERE */
-    numberElements ++;
-  }//end add()
+    numberElements++;
 
+  }// end add()
 
-  public boolean isEmpty()
-  {
-    //return whether this SuperArray instance is empty
-    return numberElements==0; //(is numberElements equal to 0 or not)
-    /*if (numberElements ==0){
-      
-      return true;
-    } else {
-        return false;
-    }*/
+  public boolean isEmpty() {
+    // return whether this SuperArray instance is empty
+    return numberElements == 0; // (is numberElements equal to 0 or not)
+    /*
+     * if (numberElements ==0){
+     * 
+     * return true; } else { return false; }
+     */
   }
 
-
-  public int get(int index)
-  {
-    //return item at index
+  public int get(int index) {
+    // return item at index
     return data[index];
   }
 
-
   /*
-  //return stringified version of this Object
-  public String toString()
-  {
-    String s = "";
-    for (int i =0; i < numberElements; i++) {
-      s = s + data[i] + ", ";
-    }                              
-    s = s + "\n";
-    return s;
-  }//end toString()
-  */
+   * //return stringified version of this Object public String toString() { String
+   * s = ""; for (int i =0; i < numberElements; i++) { s = s + data[i] + ", "; } s
+   * = s + "\n"; return s; }//end toString()
+   */
 
-  //return Stringified version of this Object,
+  // return Stringified version of this Object,
   // with extra debugging info
-  //(helper method for debugging/development phase)
-  public String debug()
-  {
+  // (helper method for debugging/development phase)
+  public String debug() {
     String s = "";
     s = "Size: " + this.data.length;
     s = s + " LastItem: " + numberElements + "  Data: ";
@@ -129,54 +96,65 @@ public class SuperArray
     }
     s = s + "\n";
     return s;
-  }//end debug()
+  }// end debug()
 
-
-  public void remove(int index)
-  { // shift items down to remove the item at valid index
+  public void remove(int index) { 
+    
+    //first check if the index if valid
     if (0 > index || index >= numberElements) {
       System.out.println("Invalid index #");
+
+    // shift items down to remove the item at valid index
     } else {
-        for (int i = index; i <numberElements-1; i++){
-          data[i] = data[i+1];
-        }
-    // subtract from numElements;
-        numberElements = numberElements -1;
+
+      //start looping at the index to override the index
+      //end looping at numElements - 1 because we're shrinking the array by 1
+      for (int i = index; i < numberElements - 1; i++) {
+
+        //take item from the right and store it to the left
+        data[i] = data[i + 1];
+      }
+      
+      // subtract from numElements
+        numberElements = numberElements - 1;
       }
   }
 
-
-  public void add(int index, int value)
-  {
+  public void add(int index, int value) {
+   
     // see if there's enough room
-    /* YOUR SIMPLE+SMART CODE HERE */
+    if (numberElements == data.length) {   //narrowest condition (not including >)
+      grow();
+    }
 
     // shift elements toward the end of the array
-    /* YOUR SIMPLE+SMART CODE HERE */
-
-    // insert new element
-    /* YOUR SIMPLE+SMART CODE HERE */
-
-    // increment numElements
-    /* YOUR SIMPLE+SMART CODE HERE */
-  }
-
-
-  private void grow()
-  {
-    // create a new array with extra space
-    // Q: How did you decide how much to increase capacity by?
-    int[] data1 = new int [data.length+1];
-
-    // copy over all the elements from the old array (data) to the new one (data1)
-    for (int i =0; i < data.length; i++){
-      data1[i]=data[i];
+    //need to shift backwards to avoid erasing elements
+    for (int i= numberElements-1;i>=index ; i--){ //need to shift the element at index
+      data[i+1]=data[i];
     }
     
+    // insert new element
+    data[index] = value;
+
+    // increment numberElements
+    numberElements++;
+
+  }
+
+  private void grow() {
+    // create a new array with extra space
+    // Q: How did you decide how much to increase capacity by?
+    int[] data1 = new int[data.length + 1];
+
+    // copy over all the elements from the old array (data) to the new one (data1)
+    for (int i = 0; i < data.length; i++) {
+      data1[i] = data[i];
+    }
+
     // point data to the new array (data1)
     // Q: How does this look when illustrated using encapsulation diagram?
-    data = data1; //now data is the same as data1 lengthwise
-         
-  }//end grow()
+    data = data1; // now data is the same as data1 lengthwise
 
-}//end class
+  }// end grow()
+
+}// end class
--- programming/6/sa/SuperArrayDriver.java
+++ programming/6/sa/SuperArrayDriver.java
@@ -1,58 +1,66 @@
-import java.io.*;
-import java.util.*;
-
-public class SuperArrayDriver
-{
-  public static void main( String[] args )
+  import java.io.*;
+  import java.util.*;
+  
+  public class SuperArrayDriver
   {
-    
-    System.out.println("Testing default constructor.");
-    SuperArray sa = new SuperArray(10);
-    System.out.println("Testing empty print:");
-   //System.out.println(sa.debug());
-    System.out.println("Testing isEmpty (should be true)");
-    System.out.println(sa.isEmpty());
-    
-    System.out.println(sa);
+    public static void main( String[] args )
+    {
+      
+      System.out.println("Testing default constructor.");
+      SuperArray sa = new SuperArray(10);
+      //System.out.println("Testing empty print:");
+      System.out.println("\nMemory location of sa:");
+      System.out.println(sa);
+      System.out.println("\n Testing isEmpty (should be true)");
+      System.out.println(sa.isEmpty());
+      
+      //System.out.println(sa);
+  
+      //testing out array locations in memory
+      SuperArray sa2 = new SuperArray(12);
+      System.out.println("\nMemory location of sa2:");
+      System.out.println(sa2);
+      sa = sa2;
+      System.out.println("\nMemory location of sa after sa = sa2:");
+      System.out.println(sa);
+  
+      // -----------------------------------------------------------
+      // YOUR HOMEGROWN/HOUSEMADE/ROLL-YOUR-OWN TEST METHODS HERE...
+      // -----------------------------------------------------------
+      //precondition: newly-instantiated SuperArray of default capacity
+      System.out.println("\nTesting overfill (calling grow() in add)");
+      for (int i=0; i<15; i++) {
+        sa.add(i);
+      }//for i
+   
+      System.out.print("Is sa empty now? " + sa.isEmpty());
+      System.out.println(sa.debug());
+      
+  
+      //test the get() accessor method
+      System.out.println("What is in element 14? " + sa.get(14));
+  
+      
+  
+      
+      System.out.println("\nTesting add at index 5, 0 and 17(end)");
+      sa.add(5, 100);
+      System.out.println(sa.debug());
+      sa.add(0, 100);
+      System.out.println(sa.debug());
+      sa.add(17, 100);
+      System.out.println(sa.debug());
+     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-    SuperArray sa2 = new SuperArray(12);
-    System.out.println(sa2);
+      System.out.println("\nTesting remove at 0, 5, and 15 ");
+      sa.remove(0);
+      System.out.println(sa.debug());
+      sa.remove(5);
+      System.out.println(sa.debug());
+      sa.remove(15);
+      System.out.println(sa.debug());
 
-    sa = sa2;
-
-    System.out.println(sa);
-
-    
-
-    // -----------------------------------------------------------
-    // YOUR HOMEGROWN/HOUSEMADE/ROLL-YOUR-OWN TEST METHODS HERE...
-    // -----------------------------------------------------------
-    //precondition: newly-instantiated SuperArray of default capacity
-    System.out.println("Testing overfill (calling grow() in add)");
-    for (int i=0; i<15; i++) {
-      sa.add(i);
-    }//for i
-    System.out.println(sa);
-    System.out.println(sa.isEmpty());
-
-
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    System.out.println("\nTesting add at index 5, 0 and 17(end)");
-    sa.add(5, 100);
-    System.out.println(sa);
-    sa.add(0, 100);
-    System.out.println(sa);
-    sa.add(17, 100);
-    System.out.println(sa);
-    System.out.println("\nTesting remove at 0, 5, and 15 (end)");
-    sa.remove(0);
-    System.out.println(sa);
-    sa.remove(5);
-    System.out.println(sa);
-    sa.remove(15);
-    System.out.println(sa);
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-  }//main
-
-}//class SuperArrayDriver
+  
+    }//main
+  
+  }//class SuperArrayDriver
